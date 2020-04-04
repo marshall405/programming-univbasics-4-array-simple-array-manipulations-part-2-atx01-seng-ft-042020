70fe702e02a3e09c539f29c59cb3e4866193175e
Marshall Slemp <marshall.slemp@gmail.com>
2020-04-04 17:27:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,21 +3,21 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert(4,element)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
-  
+  array.uniq()
 end
 
 def using_flatten(array)
-  
+  array.flatten()
 end
 
 def using_delete(array, str)
-  
+  array.delete(str)
 end
 
 def using_delete_at(array, int)
-  
+  array.delete_at(int)
 end
